440dd998f1ff30b1acc98a3f92dfbf0237140c68
Remedios Sibug <112613658+MhieRhemz79@users.noreply.github.com>
2022-11-26 09:19:18 +0800

    Add files via upload

--- java.js
+++ java.js
@@ -1,7 +1,7 @@
 let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let navmenu = document.querySelector('.navmenu');
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
+    navmenu.classList.toggle('open');
 }
